e41cb224fc9c4e019c2a58a6e1dc7ed8702e20c9
Hiromi Takahashi <hiromix222@gmail.com>
2020-12-21 00:49:08 +0900

    first commit

---
+++ src/main/java/com/example/jeedemo/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.jeedemo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
