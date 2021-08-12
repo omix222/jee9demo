cb8a357f7b35bae8d0066fa31ff78d61485ee828
omix222 <omix222@users.noreply.github.com>
2021-08-12 17:07:28 +0900

    Github上でのVSCコミットおためし

--- src/main/java/com/example/jeedemo/HelloResource.java
+++ src/main/java/com/example/jeedemo/HelloResource.java
@@ -3,12 +3,14 @@ package com.example.jeedemo;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-
 @Path("/hello-resource")
 public class HelloResource {
+
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World! from HelloResource!!";
+        String str = "Hello";
+
+        return str + ", World! from HelloResource!!";
     }
 }
